aeac9eb12ba3ba938e7a3171c9ce847670b27080
Rocky Pukar <ckrp2018@gmail.com>
2024-10-19 18:19:53 +0545

    feat(admin): add modal components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,17 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { useEffect } from "react";
 
 export default function SetupPage() {
-  return (
-    <div className="p-4">
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen,onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
