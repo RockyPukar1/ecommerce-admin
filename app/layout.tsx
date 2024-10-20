ae5c3bb3dbd3b60ee90f3b91cd486756f596c699
Rocky Pukar <ckrp2018@gmail.com>
2024-10-21 00:02:30 +0545

    feat(nav): implement navigation bar for admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <ToasterProvider />
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { MainNav } from "@/components/MainNav";
+import StoreSwitcher from "@/components/StoreSwitcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+export const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
+      </div>
+    </div>
+  );
+};
